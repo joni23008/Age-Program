ade6ae4d92f7b742ce51225a9b120a57a0887b49
joni23008 <joni.mitronen@student.hamk.fi>
2023-09-19 11:41:52 +0300

    chanced program from finnish to english

--- AgeProgram_Github/src/AgeProgram.java
+++ AgeProgram_Github/src/AgeProgram.java
@@ -8,11 +8,11 @@ public class AgeProgram {
 		
 		Scanner in = new Scanner(System.in);
 		
-		int ika = 14;
+		int ika;
 		String ikasana;
 		
 
-		System.out.println("Kirjoita ikäsi");
+		System.out.println("Type your age");
 		ikasana = in.nextLine();
 		ika = Integer.parseInt(ikasana);
 
@@ -23,50 +23,50 @@ public class AgeProgram {
 		if (ika >= 0 && ika < 18)
 		{
 
-			System.out.println("Olet alaikäinen" + "\n");
+			System.out.println("You are a minor" + "\n");
 			if (ika >= 15)
 			{
-					System.out.println("Saat ajaa ikäsi puolesta seuraavia ajoneuvoja" + "\n");
-					System.out.println("-Mopo");
+					System.out.println("You are qualified by your age to drive these vehicles " + "\n");
+					System.out.println("-Moped");
 					
 			}
 				if (ika >= 16)
 				{
-						System.out.println("-Kevari");
+						System.out.println("-Lightmotorcycle");
 				}
 		}
 		
 		
 		else if (ika >= 18 && ika < 65)
 		{
-			System.out.println("Olet aikuinen" + "\n");
+			System.out.println("You are a adult" + "\n");
 			if (ika >= 18)
 			{
-				System.out.println("Saat ajaa ikäsi puolesta seuraavia ajoneuvoja" + "\n");
-				System.out.println("-Mopo");
-				System.out.println("-Kevari");
-				System.out.println("-Auto");
+				System.out.println("You are qualified by your age to drive these vehicles " + "\n");
+				System.out.println("-Moped");
+				System.out.println("-Lightmotorcycle");
+				System.out.println("-Car");
 			}
 				if (ika >= 40 && ika <= 50)
 				{
-					System.out.println("\n" + "Hyvää keski-ikää!");
+					System.out.println("\n" + "Happy middle age!");
 				}
 					if (ika >= 58 && ika <65)
 					{	
-							System.out.println("\n" + "Voit mennä varhaiseläkkeelle.");
+							System.out.println("\n" + "You can take early retirement.");
 					}
 			
 		}
 		else 
 		{
-			System.out.println("Olet eläkeläinen" + "\n");
+			System.out.println("You are pensioner" + "\n");
 			if (ika >= 65)
 			{	
-				System.out.println("Saat ajaa ikäsi puolesta seuraavia ajoneuvoja" + "\n");
-				System.out.println("-Mopo");
-				System.out.println("-Kevari");
-				System.out.println("-Auto");
-				System.out.println("\n" + "Hyviä eläkepäiviä!");
+				System.out.println("You are qualified by your age to drive these vehicles " + "\n");
+				System.out.println("-Moped");
+				System.out.println("-Lightmotorcycle");
+				System.out.println("-Car");
+				System.out.println("\n" + "Happy retiremnt!");
 				
 			}
 			
@@ -75,38 +75,39 @@ public class AgeProgram {
 		
 		//Syntymäpäivä onnittelut
 		
-				switch (ika) {
+				switch (ika)
+				{
 				case 10:
-					System.out.println ("Onneksi olkoon,täytit " + ika + "v!");
+					System.out.println ("Congratulations on your " + ika + "rd birthday");
 					break;
 				case 20:
-					System.out.println ("Onneksi olkoon,täytit " + ika + "v!");
+					System.out.println ("Congratulations on your " + ika + "rd birthday");
 					break;
 				case 30:
-					System.out.println ("Onneksi olkoon,täytit " + ika + "v!");
+					System.out.println ("Congratulations on your " + ika + "rd birthday");
 					break;
 				case 40:
-					System.out.println ("Onneksi olkoon,täytit " + ika + "v!");
+					System.out.println ("Congratulations on your " + ika + "rd birthday");
 					break;
 				case 50:
-					System.out.println ("Onneksi olkoon,täytit " + ika + "v!");
+					System.out.println ("Congratulations on your " + ika + "rd birthday");
 					break;
 				case 60:
-					System.out.println ("Onneksi olkoon,täytit " + ika + "v!");
+					System.out.println ("Congratulations on your " + ika + "rd birthday");
 					break;
 				case 70:
-					System.out.println ("Onneksi olkoon,täytit " + ika + "v!");
+					System.out.println ("Congratulations on your " + ika + "rd birthday");
 					break;
 				case 80:
-					System.out.println ("Onneksi olkoon,täytit " + ika + "v!");
+					System.out.println ("Congratulations on your " + ika + "rd birthday");
 					break;
 				case 90:
-					System.out.println ("Onneksi olkoon,täytit " + ika + "v!");
+					System.out.println ("Congratulations on your " + ika + "rd birthday");
 					break;
 				case 100:
-					System.out.println ("		ONNEKSI OLKOON");
-					System.out.println ("		    TÄYTIT");
-					System.out.println ("	             "+ ika + "V!");
+					System.out.println ("	GONGRATULATIONS ON YOUR");
+					System.out.println ("                 " + ika);
+					System.out.println ("	      RD BIRTHDAY!");
 					break;
 				}
 		
