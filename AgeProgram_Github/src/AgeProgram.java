ae480ee39af0709aaabee98a3a189a5f8a89e79e
joni23008 <joni.mitronen@student.hamk.fi>
2023-09-19 11:16:51 +0300

    replace and improve. user now types the age and gets notifications based on age which were typed

--- AgeProgram_Github/src/AgeProgram.java
+++ AgeProgram_Github/src/AgeProgram.java
@@ -1,20 +1,21 @@
-
+import java.util.Scanner;
 
 public class AgeProgram {
 
 	public static void main(String[] args) {
 
-		// Tulostusehdot
-//		Tehtävä 1: Laajenna ohjelmaa siten, että ohjelma osaa kertoa 16-17-vuotiaille, että he voivat ajaa kevaria.
-//		Tehtävä 2: Laajenna ohjelmaa niin, että se kertoo 18-vuotiaalle, että hän on juuri tullut täysi-ikäiseksi ja saa ajaa autoa.
-//		Tehtävä 3: Laajenna tehtävää niin, että aikuisille ilmoitetaan tasavuosikymmenistä onnentoivotus.
-//		Tehtävä 4: jos ikä on 100, tulosta 3-rivinen onnentoivotus.
-//		Tehtävä 5: Tarkenna eläkeasioita siten, että yli 58-vuotiaille kerro, että he voivat mennä varhaiseläkkeelle.
-//		Tehtävä 6: Toivota 65-vuotiaille hyviä eläkepäiviä.
-//		Tehtävä 7: 40–50 -vuotiaille toivota parasta keski-ikää.
+//Syötä nimesi, ohjelma kertoo saatko mm. olla kuljettaja millä ajoneuvoilla ja onnittelee mm. syntymäpäivistä
 		
+		Scanner in = new Scanner(System.in);
 		
 		int ika = 14;
+		String ikasana;
+		
+
+		System.out.println("Kirjoita ikäsi");
+		ikasana = in.nextLine();
+		ika = Integer.parseInt(ikasana);
+
 		
 		//Ikä-luokat
 		
@@ -108,11 +109,6 @@ public class AgeProgram {
 					System.out.println ("	             "+ ika + "V!");
 					break;
 				}
-		
-		
-		
-		
-		
 		
 		
 
